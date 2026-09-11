031d3cb59faaf941e31873bcfc0081df23725586
HARSHAD PHULE <harshadphule7@gmail.com>
2026-09-12 00:05:09 +0530

    jenkins file

--- source-code/s3.tf
+++ source-code/s3.tf
@@ -1,8 +1,6 @@
 # S3 bucket for storing logs or other resources (optional)
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
-  acl    = "private"
-
   tags = {
     Name = "Terraform-Bucket"
   }
