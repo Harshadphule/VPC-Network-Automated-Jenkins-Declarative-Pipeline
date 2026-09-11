5d5eb5d7f4d46fd2d12406fb8cc02912a3721a05
HARSHAD PHULE <harshadphule7@gmail.com>
2026-09-12 00:00:43 +0530

    jenkins file

--- source-code/backend.tf
+++ source-code/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-vpc-state-bucket-freestyle-1"
+    bucket = "terraform-vpc-state-bucket-declerative-1"
     key    = "remotedemo.tfstate"
     region = "ap-south-1"
   }
